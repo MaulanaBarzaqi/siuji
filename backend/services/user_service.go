8cd7fb789cab35351aeae7f0b408af6e9c18f5b6
Maulana <molaawoii@gmail.com>
2026-08-14 15:47:29 +0700

    feat(backend/services): add method user_service: get user by publicID and delete user

--- backend/services/user_service.go
+++ backend/services/user_service.go
@@ -1,13 +1,15 @@
 package services
 
 import (
+	"errors"
 	"siuji-backend/models"
 	"siuji-backend/repositories"
 )
 
 type UserService interface {
 	GetAllPagination(filter, sort string, limit, offset int) ([]models.User, int64, error)
-	// DeleteParticipant(userID uint) error
+	GetUserByPublicID(publicID string) (*models.User, error)
+	DeleteUser(publicID string) error
 }
 
 type userService struct {
@@ -22,10 +24,18 @@ func (s *userService) GetAllPagination(filter, sort string, limit, offset int) (
 	return s.userRepo.FindAllPagination(filter, sort, limit, offset)
 }
 
-// func (s *userService) DeleteParticipant(userID uint) error {
-// 	_, err := s.userRepo.FindByID(userID)
-// 	if err != nil {
-// 		return errors.New("user not found")
-// 	}
-// 	return s.userRepo.Delete(userID)
-// }
+func (s *userService) GetUserByPublicID(publicID string) (*models.User, error) {
+	user, err := s.userRepo.FindByPublicID(publicID)
+	if err != nil {
+		return nil, errors.New("user not found")
+	}
+	return user, nil
+}
+
+func (s *userService) DeleteUser(publicID string) error {
+	_, err := s.userRepo.FindByPublicID(publicID)
+	if err != nil {
+		return errors.New("user not found")
+	}
+	return s.userRepo.Delete(publicID)
+}
